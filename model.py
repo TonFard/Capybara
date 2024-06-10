ec4dea63d0aba5b45f83c1b974b72bb04bd6e5bb
TonFard <77236666+TonFard@users.noreply.github.com>
2024-06-10 20:00:18 +0800

    Update model.py

--- model.py
+++ model.py
@@ -25,20 +25,27 @@ class ModelConfig:
     
 
 class CapybaraRMSNorm(nn.Module):
-    def __init__(self, hidden_size: int, norm_eps: float=1e-6):
+    def __init__(self, hidden_size: int, norm_eps: float=1e-6, bias: bool=False):
         """
         CapybaraRMSNorm
         """
         super(CapybaraRMSNorm, self).__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = norm_eps
-        
-    def forward(self, hidden_states: torch.Tensor) -> torch.Tensor:
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        return self.weight * hidden_states.to(input_dtype)
+        if bias:
+            self.bias = nn.Parameter(torch.empty(hidden_size))
+        else:
+            self.register_parameter("bias", None)
+                    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        input_dtype = x.dtype
+        x = x.to(torch.float32)
+        variance = x.pow(2).mean(-1, keepdim=True)
+        x = x * torch.rsqrt(variance + self.variance_epsilon)
+        print(self.bias)
+        if self.bias is not None:
+            x = x + self.bias.to(input_dtype)
+        return self.weight * x.to(input_dtype)
 
 
 # class CapybaraRotaryEmbedding():
