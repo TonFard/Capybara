03ddd7fd26fcee2b1ffb7ac605005bdfddec05a5
TonFard <77236666+TonFard@users.noreply.github.com>
2024-06-17 16:14:12 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,74 +5,75 @@ import torch.nn.functional as F
 import math
 from dataclasses import dataclass, field
 from typing import List, Tuple, Dict, Optional, Union
-
+nn.LayerNorm
 
 @dataclass
 class ModelConfig:
-    hidden_size: int = field(default=768)
-    intermediate_size: Optional[int] = field(default=None)
-    num_hidden_layers: int = field(default=12)  # Number of layers in the transformer
-    num_attention_heads: int = field(default=12)  # Number of attention heads
-    n_kv_heads: Optional[int] = field(default=None)  # Number of key-value heads (optional, defaults to n_heads)
-    attention_dropout: float = field(default=0.0)
-    mlp_dropout: float = field(default=0.0)
-    bias: bool = field(default=False)
-
-    vocab_size: int = field(default=16666)  # Vocabulary size
-    block_size: int = field(default=1024)
-    norm_eps: float = field(default=1e-6)  # Epsilon value for normalization
-    max_batch_size: int = field(default=32)  # Maximum batch size for training
-    max_seq_len: int = field(default=2048)  # Maximum sequence length
+    attention_bias: bool = False
+    attention_dropout: float = 0.0
+    # bos_token_id: int = 1
+    # eos_token_id: int = 2
+    hidden_size: int = 768
+    hidden_act: str = "gelu"
+    ffn_bias: bool = False
+    intermediate_size: int = (int(hidden_size * 8/3 / 128) + 1) * 128
+    num_attention_heads: int = 16
+    num_hidden_layers: int = 4
+    num_kv_heads: int = 8
+    rms_norm_eps: float = 1e-6
+    rope_theta: float = 10000.0
+    vocab_size: int = 64794
+    block_size: int = 1024
+    
+    def __repr__(self):
+        attributes = vars(self)
+        lines = []
+        for attr, value in attributes.items():
+            lines.append(f"\n    {attr}: {value}")
+        return f"{self.__class__.__name__}({','.join(lines)}\n)"
     
 
-class CapybaraRMSNorm(nn.Module):
-    def __init__(self, hidden_size: int, norm_eps: float=1e-6, bias: bool=False):
+class RMSNorm(nn.Module):
+    def __init__(
+        self, 
+        hidden_size: int, 
+        norm_eps: float=1e-6,
+    ):
         """
         CapybaraRMSNorm
         """
-        super(CapybaraRMSNorm, self).__init__()
+        super(RMSNorm, self).__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = norm_eps
-        if bias:
-            self.bias = nn.Parameter(torch.empty(hidden_size))
-        else:
-            self.register_parameter("bias", None)
                     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         input_dtype = x.dtype
         x = x.to(torch.float32)
         variance = x.pow(2).mean(-1, keepdim=True)
         x = x * torch.rsqrt(variance + self.variance_epsilon)
-        print(self.bias)
-        if self.bias is not None:
-            x = x + self.bias.to(input_dtype)
         return self.weight * x.to(input_dtype)
 
 
-# class CapybaraRotaryEmbedding():
-    # raise NotImplementedError("Reference DeepSeedv2")
-
-
-class CapybaraAttention(nn.Module):
+class CasualSelfAttention(nn.Module):
     def __init__(
         self, 
         hidden_size: int,
         num_attention_heads: int,
         attention_dropout: float = 0.0,
-        bias: bool = False
+        attention_bias: bool = False
     ):
-        super(CapybaraAttention, self).__init__()
+        super(CasualSelfAttention, self).__init__()
         self.hidden_size = hidden_size
         self.num_heads = num_attention_heads
         self.attention_dropout = attention_dropout
-        self.wqkv = nn.Linear(hidden_size, 3 * hidden_size, bias=bias)
-        self.proj = nn.Linear(hidden_size, hidden_size, bias=bias)
+        self.wqkv = nn.Linear(hidden_size, 3 * hidden_size, bias=attention_bias)
+        self.proj = nn.Linear(hidden_size, hidden_size, bias=attention_bias)
         self.attn_dropout = nn.Dropout(attention_dropout)
         self.q_head_dim = self.hidden_size // self.num_heads
 
         self.proj_dropout = nn.Dropout(attention_dropout)
         self.softmax_scale = self.q_head_dim ** -0.5
-
+        
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         bsz, seq_len, hidden_size = x.size()  # [batch_size, sequence_length, embedding dimension]
         q, k, v = self.wqkv(x).split(self.hidden_size, dim=2)
@@ -89,83 +90,86 @@ class CapybaraAttention(nn.Module):
         return attn_output
 
 
-class CapybaraMLP(nn.Module):
+class MLP(nn.Module):
     def __init__(
         self,
         hidden_size: int,
         intermediate_size: Optional[int] = None,
+        act: str = 'gelu',
         bias: bool = False,
-        mlp_dropout: float = 0.0
     ):
         """
-        CapybaraMLP reference nanoGPT MLP and DeepSeekv2 MLP
+        MLP reference nanoGPT MLP and DeepSeekv2 MLP
         """
-        super(CapybaraMLP, self).__init__()
+        super(MLP, self).__init__()
         if intermediate_size is None:
-            intermediate_size = (int(hidden_size * 8/3 / 128) + 1) * 128
+            intermediate_size = int((int(hidden_size * 8/3 / 128) + 1) * 128)
         self.in_proj = nn.Linear(hidden_size, intermediate_size, bias=bias)
-        self.act = nn.GELU()
+        if act.lower() == "gelu":
+            self.act = nn.GELU() 
+        elif act.lower() == "silu":
+            self.act = nn.SiLU()
+        else:
+            NotImplementedError(f"not support {act} !!!")
         self.out_proj = nn.Linear(intermediate_size, hidden_size, bias=bias)
-        self.dropout = nn.Dropout(mlp_dropout)
 
     def forward(self, x: torch.Tensor):
         x = self.in_proj(x)
         x = self.act(x)
         x = self.out_proj(x)
-        x = self.dropout(x)
         return x
 
 
-class CapybaraDecoderLayer(nn.Module):
+class DecoderLayer(nn.Module):
     def __init__(
         self, 
         hidden_size: int,
         intermediate_size: Optional[int], 
         num_attention_heads: int, 
         attention_dropout: float, 
-        mlp_dropout: float, 
-        norm_eps: float, 
-        bias: bool
+        attention_bias: bool,
+        norm_eps: float,
+        ffn_bias: bool,
+        act: str
     ):
         """
-            CapybaraDecoderLayer
+            DecoderLayer
         """
-        super(CapybaraDecoderLayer, self).__init__()
-        self.norm1 = CapybaraRMSNorm(hidden_size, norm_eps)
-        self.attn = CapybaraAttention(hidden_size, num_attention_heads, attention_dropout, bias)
-        self.norm2 = CapybaraRMSNorm(hidden_size, norm_eps)
-        self.mlp = CapybaraMLP(hidden_size, intermediate_size, bias, mlp_dropout)
+        super(DecoderLayer, self).__init__()
+        self.norm1 = RMSNorm(hidden_size, norm_eps)
+        self.attn = CasualSelfAttention(hidden_size, num_attention_heads, attention_dropout, attention_bias)
+        self.norm2 = RMSNorm(hidden_size, norm_eps)
+        self.mlp = MLP(hidden_size, intermediate_size, act, ffn_bias)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor: 
         x = x + self.attn(self.norm1(x))
         x = x + self.mlp(self.norm2(x))
         return x
     
-
+    
 class Capybara(nn.Module):
     def __init__(self, config):
         super(Capybara, self).__init__()
         self.config = config
-
         self.wte = nn.Embedding(config.vocab_size, config.hidden_size)
         self.pte = nn.Embedding(config.block_size, config.hidden_size)
-        self.dropout = nn.Dropout(config.mlp_dropout)
 
         self.layers = nn.ModuleList(
             [
-                CapybaraDecoderLayer(
+                DecoderLayer(
                     config.hidden_size,
                     config.intermediate_size,
                     config.num_attention_heads,
                     config.attention_dropout,
-                    config.mlp_dropout,
-                    config.norm_eps,
-                    config.bias
+                    config.attention_bias,
+                    config.rms_norm_eps,
+                    config.ffn_bias,
+                    config.hidden_act
                 )
                 for _ in range(config.num_hidden_layers)
             ]
         )
-        self.norm = CapybaraRMSNorm(config.hidden_size)
+        self.norm = RMSNorm(config.hidden_size)
         self.fc = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
 
     def forward(self, idx, target=None):
@@ -176,7 +180,7 @@ class Capybara(nn.Module):
 
         tok_emb = self.wte(idx)
         pos_emb = self.pte(pos)
-        x = self.dropout(tok_emb + pos_emb)
+        x = tok_emb + pos_emb
 
         for layer in self.layers:
             x = layer(x)
@@ -190,13 +194,13 @@ class Capybara(nn.Module):
             loss = None
         return logits, loss
 
-    def crop_block_size(self, block_size):
-        assert block_size <= self.config.block_size
-        self.config.blcok_size = block_size
-        self.wpe.weight = nn.Parameter(self.wpe.weight[:block_size])
-        for block in self.layers:
-            if hasattr(block.attn, "bias"):
-                block.attn.bias = block.attn.bias[:, :, :block_size, :block_size]
+    # def crop_block_size(self, block_size):
+    #     assert block_size <= self.config.block_size
+    #     self.config.blcok_size = block_size
+    #     self.wpe.weight = nn.Parameter(self.wpe.weight[:block_size])
+    #     for block in self.layers:
+    #         if hasattr(block.attn, "bias"):
+    #             block.attn.bias = block.attn.bias[:, :, :block_size, :block_size]
     
     @torch.no_grad()
     def generate(self, idx, max_new_token, temperature=1.0, top_k=None):
@@ -204,7 +208,7 @@ class Capybara(nn.Module):
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size]
             logits, _ = self(idx_cond)
             logits = logits[:, -1, :] / temperature
-
+        
             if top_k is not None:
                 v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
                 logits[logits < v[:, [-1]]] = -float("Inf")
@@ -214,9 +218,6 @@ class Capybara(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
         return idx
     
-
-
-
 
 # 打印模型的每一层及其参数大小
 def print_model_parameters(model):
@@ -234,10 +235,8 @@ def print_model_parameters(model):
 
 if __name__ == "__main__":
     config = ModelConfig()
-    batch_size = config.max_batch_size
-    seq_length = config.max_seq_len
     hidden_size = config.hidden_size
-    input_tensor = torch.rand(batch_size, seq_length, hidden_size)
+    input_tensor = torch.rand(32, 10, hidden_size)
     model = Capybara(config)
     print_model_parameters(model)
 
